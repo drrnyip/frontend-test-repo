4a3b86286504ced8556377570896cc3701959ead
Darren Yip <drrnyip@gmail.com>
2018-12-09 21:51:38 -0500

    feat(serverCalls): Make server calls, and add success/failure states

--- src/app/components/refill/refill.component.ts
+++ src/app/components/refill/refill.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MockServerService } from '../../services/mock-server.service';
 
 @Component({
   selector: 'app-refill',
@@ -9,21 +10,38 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RefillComponent implements OnInit {
 
-  public operator: String = '';
+  public operator = '';
   public refillForm: FormGroup;
+  public state = 'refilling';
+  public remainingBalance = 0;
 
-  constructor(private _router: Router, private _activatedRoute: ActivatedRoute, private _fb: FormBuilder) { }
+  @ViewChild('returnBtn') returnBtn;
+  @ViewChild('confirmBtn') confirmBtn;
+
+  constructor(
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+    private _fb: FormBuilder,
+    private _mockServer: MockServerService
+  ) { }
 
   ngOnInit() {
     this.operator = this._activatedRoute.snapshot.params['operator'];
+    if (!this._mockServer.verifyOperator(this.operator)) {
+      this.returnToOperator();
+    }
     this.refillForm = this._fb.group({
       phone: ['', [Validators.required]],
-      amount: ['', Validators.required]
+      amount: ['', [Validators.required]]
     });
   }
 
   public returnToOperator() {
     this._router.navigate(['/', 'operators']);
+  }
+
+  public returnToRefillPage() {
+    this._changeState('refilling');
   }
 
   public confirmRefill() {
@@ -41,20 +59,29 @@ export class RefillComponent implements OnInit {
       }
 
       if (!errors) {
-        // Disable inputs
-        // this.refillForm.disable();
-        
+        this._changeState('loading');
+        // Send calls to mock server
+        this._mockServer.refillAccount(this.operator, phoneNumber, refillAmount).then((data: any) => {
+          if (data) {
+            this._changeState('success');
+            this.remainingBalance = data.amount;
+            setTimeout(() => {
+              this.returnToOperator();
+            }, 5000);
+          }
+        }).catch((e) => {
+          console.log(e);
+          this._changeState('failure');
+        });
       }
     }
   }
 
   public maskAmount(event) {
-    console.log(this.refillForm);
     // Currently code does not allow breaking up a ruble.
     const allowedKeycodes = [8];
     if (event.key.match(/^\d+$/)) {
       const amount = this.refillForm.controls.amount.value;
-      console.log('amount: ' + amount);
       const mask = amount.replace(/\D/g, '');
       if (mask + event.key <= 1000 && mask + event.key >= 0) {
         this.refillForm.setValue({
@@ -80,6 +107,10 @@ export class RefillComponent implements OnInit {
       event.preventDefault();
       return;
     }
+  }
+
+  private _changeState(state) {
+    this.state = state;
   }
 
 
--- src/app/services/mock-server.service.ts
+++ src/app/services/mock-server.service.ts
@@ -20,4 +20,36 @@ export class MockServerService {
     this._operators[name] = accounts;
     return this._operators[name];
   }
+
+  public verifyOperator(name: string) {
+    return this._operators[name] ? true : false;
+  }
+
+  public refillAccount(operator: string, number: string, amount: number) {
+    return new Promise((res, rej) => {
+      // Two second delay to simulate server response time.
+      setTimeout(() => {
+        if (!this._randomSuccess()) {
+          rej('Request failed');
+        }
+        if (this._operators[operator]) {
+          const activeOperator = this._operators[operator];
+          if (!activeOperator[number]) {
+            activeOperator[number] = 0;
+          }
+          activeOperator[number] += amount;
+        }
+        res({
+          operator,
+          number,
+          amount
+        });
+      }, 2000);
+    });
+  }
+
+  private _randomSuccess() {
+    // Simulating random success and error cases
+    return Math.random() > 0.5;
+  }
 }
